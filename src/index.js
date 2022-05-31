ffdfb079049594e17c62919416c86677b53a1884
Vika <viktoriav.aero@gmail.com>
2022-05-31 21:36:18 +0300

    added bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,69 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createStore} from 'redux';
+import {createStore, bindActionCreators} from 'redux';
+import reducer from './reducer';
+import * as actions from './action';
 
-const initialState = {value: 0};
-
-const reducer = (state = initialState, action) => {
-
-  switch (action.type) {
-    case "INC": 
-      return {
-        ...state,
-        value: state.value + 1
-      }
-    case "DEC": 
-      return {
-        ...state,
-        value: state.value + 1
-      }
-    case "RND": 
-      return {
-        ...state,
-        value: state.value * action.payload
-      }
-    default: 
-      return state;
-  }
-}
 
 const store = createStore(reducer);
 
+const {dispatch, subscribe, getState} = store;
+
 const update = () => {
-  document.getElementById('counter').textContent = store.getState().value;
+  document.getElementById('counter').textContent = getState().value;
 }
 
-store.subscribe(update);
+subscribe(update);
 
-const inc = () => ({type: 'INC'});
-const dec = () => ({type: 'DEC'});
-const rnd = (value) => ({type: 'RND', payload: value});
+// const incDispatch = () => dispatch(inc());
+// const decDispatch = () => dispatch(dec());
+// const rndDispatch = (value) => dispatch(rnd(value));
 
-store.subscribe(() => {
-  console.log(store.getState());
-})
 
-document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch(inc());
-})
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// }
 
-document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec());
-})
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
+// const incDispatch = bindActionCreators(inc, dispatch);
+// const decDispatch = bindActionCreators(dec, dispatch);
+// const rndDispatch = bindActionCreators(rnd, dispatch);
+
+document.getElementById('inc').addEventListener('click', inc);
+
+document.getElementById('dec').addEventListener('click', dec)
 
 document.getElementById('rnd').addEventListener('click', () => {
   const value = Math.floor(Math.random() * 10);
-  store.dispatch(rnd(value));
+  rnd(value);
 })
-
-
-
-// let state = reducer(initialState, {type: 'INC'});
-//   state = reducer(state, {type: 'INC'});
-//   state = reducer(state, {type: 'INC'});
-//   state = reducer(state, {type: 'INC'});
-
-// console.log(state);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
